0c704908ff5d9e8ae0de2da76d337d86bc696ad4
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-02-06 11:37:20 +0100

    linter setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    ignorePatterns: [".eslintrc.js"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:node/recommended-module",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+    ],
+    parserOptions: {
+        project: ["./tsconfig.json"],
+    },
+    rules: {
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
+};
--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as compute from "@pulumi/google-native/compute/v1";
+
+const server = new compute.Instance("development-server", {});
